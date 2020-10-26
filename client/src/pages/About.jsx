ce5f6af18e55220f2c8809525919a6faa896b007
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-26 18:19:37 +0530

    Modification ++

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,28 @@ const About = () => {
   return (
     <Fragment>
       <Navbar />
-      About Page
+      <div className="container text-center">
+        <h2 className="mt-4">Page Under Construction</h2>
+
+        <img
+          alt="404"
+          src="https://res.cloudinary.com/dnja3kt1q/image/upload/v1603704820/logo/maintenance_a5jtvi.png"
+          width="40%"
+        />
+        <br />
+
+        <div className="w-50 bg-danger text-left">
+          <p className="mt-4">
+            Signup/Signin with PERN Stack
+            <ul>
+              <li>PostgreSQL</li>
+              <li>ExpressJs</li>
+              <li>ReactJs</li>
+              <li>NodeJs</li>
+            </ul>
+          </p>
+        </div>
+      </div>
     </Fragment>
   );
 };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -3,65 +3,69 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const Signin = () => {
+  const SigninForm = () => (
+    <div className="container">
+      <div
+        className="d-flex justify-content-center  align-items-center"
+        style={{ height: "80vh" }}
+      >
+        <form className="p-5 rounded bg-primary">
+          <h2 className="text-light mb-5 text-center">
+            <ins>Sign In</ins>
+          </h2>
+          <div className="input-group mb-3">
+            <div className="input-group-append">
+              <span className="input-group-text">
+                <i className="fa fa-at"></i>
+              </span>
+            </div>
+            <input
+              type="email"
+              // name=""
+              className="form-control"
+              // value=""
+              placeholder="Enter Email"
+            />
+          </div>
+
+          <div className="input-group mb-5">
+            <div className="input-group-append">
+              <span className="input-group-text">
+                <i className="fa fa-key"></i>
+              </span>
+            </div>
+            <input
+              type="password"
+              // name=""
+              className="form-control input_pass"
+              // value=""
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="d-flex justify-content-center mt-3 login_container">
+            <button
+              type="submit"
+              className="btn btn-block btn-outline-light"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="text-center">
+        <h6>
+          Don't have an Account?{" "}
+          <Link to="/signup">SignUp</Link>
+        </h6>
+      </div>
+    </div>
+  );
+
   return (
     <Fragment>
       <Navbar />
-      <div className="container">
-        <div
-          className="d-flex justify-content-center  align-items-center"
-          style={{ height: "80vh" }}
-        >
-          <form className="p-5 rounded bg-primary">
-            <h2 className="text-light mb-5 text-center">
-              <ins>Sign In</ins>
-            </h2>
-            <div className="input-group mb-3">
-              <div className="input-group-append">
-                <span className="input-group-text">
-                  <i className="fa fa-at"></i>
-                </span>
-              </div>
-              <input
-                type="email"
-                // name=""
-                className="form-control"
-                // value=""
-                placeholder="Enter Email"
-              />
-            </div>
-
-            <div className="input-group mb-5">
-              <div className="input-group-append">
-                <span className="input-group-text">
-                  <i className="fa fa-key"></i>
-                </span>
-              </div>
-              <input
-                type="password"
-                // name=""
-                className="form-control input_pass"
-                // value=""
-                placeholder="Password"
-              />
-            </div>
-
-            <div className="d-flex justify-content-center mt-3 login_container">
-              <button
-                type="submit"
-                className="btn btn-block btn-outline-light"
-              >
-                Sign In
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="text-center">
-          <h6>
-            Don't have an Account?{" "}
-            <Link to="/signup">SignUp</Link>
-          </h6>
-        </div>
-      </div>
+      {SigninForm()}
     </Fragment>
   );
 };
