1858dc354589e7fee4e3d63ab935ef680fec6c0d
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-27 13:08:07 +0530

    Working on Signup.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  // <Provider store={store}>
-  <App />,
-  // </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { signup } from "../redux/actions/userActions";
+import {ToastContainer, toast} from "react-toastify"
 
-const Signup = () => {
+const Signup = ({ history, location }) => {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(
+    ""
+  );
+  const [message, setMessage] = useState(null);
+
+  const userRegister = useSelector(
+    (state) => state.userRegister
+  );
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search
+    ? location.search.split("=")[1]
+    : "/dashboard";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      dispatch(signup(name, email, password));
+    }
+  };
+
   const SignupForm = () => (
     <div className="container">
       <div
@@ -94,6 +131,10 @@ const Signup = () => {
   return (
     <Fragment>
       <Navbar />
+      <ToastContainer/>
+      {message && (
+        
+      )}
       {SignupForm()}
     </Fragment>
   );
